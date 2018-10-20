870ee684907361cb888efb58500ca277d5ae09f0
Kevin Gwynn <kevin.gwynn@gmail.com>
2018-10-20 10:07:48 -0600

    Several improvements to parsing rules

--- photo-month-folders.php
+++ photo-month-folders.php
@@ -26,23 +26,31 @@ while (true) {
 		$matches = [];
 		$year = $month = $day = $hour = $minute = $second = null;
 
+		// FIXED: Ex: 201801231058201000.jpg tries to create the path: 2023/2023-18
 		// Mostly a catch-all expression...
-		if (preg_match("/(20[012][0-9])[\. _\-]?(\d{2})[\. _\-]?(\d{2})[\. _\-](\d{2})[\. _\-]?(\d{2})[\. _\-]?(\d{2}).*?\.($extensions)$/", $file, $matches)) {
+		if (preg_match("/(20[012][0-9])[\. _\-]?(0[1-9]|1[0-2])[\. _\-]?([0-2][0-9]|3[01])[\. _\-]?([01][0-9]|2[0-3])[\. _\-]?([0-5][0-9])[\. _\-]?([0-5][0-9]).*?\.($extensions)$/", $file, $matches)) {
 			$year = $matches[1];
 			$month = $matches[2];
 			$day = $matches[3];
 			$hour = $matches[4];
 			$minute = $matches[5];
 			$second = $matches[6];
+
+//echo "Matched main expression:\n"; print_r($matches);
 		}
-		// For LG G6's stupid format, eg: 06261716.jpg (EWWW)
-		elseif (preg_match("/([01][0-9])([0-3][0-9])([12][0-9])(\d{2})(\d{2}).*?\.($extensions)$/", $file, $matches)) {
+		// For LG G6's stupid format, eg: 0626171644.jpg (EWWW)
+		elseif (preg_match("/(0[0-9]|1[0-2])([0-2][0-9]|3[01])([12][0-9])(\d{2})(\d{2}).*?\.($extensions)$/", $file, $matches)) {
 			$month = $matches[1];
 			$day = $matches[2];
 			$year = '20' . $matches[3];
 			$hour = $matches[4];
 			$minute = $matches[5];
 			$second = '00'; // not in file path
+//echo "Matched LG expression:\n"; print_r($matches);
+		}
+		// Did not match
+		else {
+			echo timestamp() . "-Could not parse: $file\n";
 		}
 
 		if ($year != null) {
